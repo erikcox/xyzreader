aeb46b46ebd2794fdadf51a7f8b4633567280566
Erik Cox <erikbcox@gmail.com>
2017-01-26 10:58:18 -0800

    Change share button from ImageButton to FAB, remove unecessary code

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -145,7 +145,7 @@ public class ArticleDetailFragment extends Fragment implements
             public void onClick(View view) {
                 startActivity(Intent.createChooser(ShareCompat.IntentBuilder.from(getActivity())
                         .setType("text/plain")
-                        .setText("Some sample text")
+                        .setText("Check out this article!")
                         .getIntent(), getString(R.string.action_share)));
             }
         });
